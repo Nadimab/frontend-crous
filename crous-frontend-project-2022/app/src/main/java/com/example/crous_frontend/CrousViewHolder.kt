000812fe238026b00a757e124ead4a64e210e1f7
Nadimab <101559862+Nadimab@users.noreply.github.com>
2022-12-04 13:24:07 +0100

    'Delete'

--- crous-frontend-project-2022/app/src/main/java/com/example/crous_frontend/CrousViewHolder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.crous_frontend
-
-import android.view.View
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
-class CrousViewHolder (rootView : View): ViewHolder(rootView){
-    var searchText = rootView.findViewById<TextView>(R.id.text_home)
-}
