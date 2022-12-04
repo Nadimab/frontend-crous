4f24b2f7682d39a90addc81a85d28f78cda32d7d
Bechara SEBAALY <becharak.sebaaly@gmail.com>
2022-12-04 16:41:19 +0100

    feat: add crous service interface

---
+++ crous-frontend-project-2022/app/src/main/java/com/example/crous/CrousService.kt
@@ -0,0 +1,33 @@
+package com.example.crous
+
+import retrofit2.Call
+import retrofit2.http.*
+
+interface CrousService {
+    @POST("crous/")
+    fun getCat(@Body crous: ExpandedCrous): Call<ExpandedCrous>;
+
+    @GET("crous/")
+    fun findAll(@Query("page") page: Int,
+                @Query("rows") rows: Int,
+                @Query("offset") offset: Int,
+                @Query("sortBy") sortBy: String,
+                @Query("fav") favorites: Int,
+                @Query("geoloc") geoloc: Int,
+    );
+
+    @GET("crous/{id}")
+    fun findOneById(@Path("id") id: String);
+
+    @POST("/search/title")
+    fun searchByTitle(@Body title: String);
+
+    @PUT("crous/{id}")
+    fun toggleFavorite(@Path("id") id: String);
+
+    @PATCH("/{id}")
+    fun update(@Path("id") id: String, @Body updateCrousDto: ExpandedCrous);
+
+    @DELETE("crous/{id}")
+    fun remove(@Path("id") id: String);
+}
